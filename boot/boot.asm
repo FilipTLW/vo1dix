7986c1b5fd58004719dbefe9fad1c28882940cec
Filip Myslinski <filipmyslinski2006@gmail.com>
2025-04-15 14:38:20 +0200

    docs: documented boot.asm

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,32 +1,33 @@
-; Multiboot Constants
-
-MBALIGN     equ   1 << 0
+MBALIGN     equ   1 << 0                            ; multiboot constants
 MEMINFO     equ   1 << 1
 MBFLAGS     equ   MBALIGN | MEMINFO
 MAGIC       equ   0x1BADB002
 CHECKSUM    equ   -(MAGIC + MBFLAGS)
 
-section .multiboot
+section .multiboot                                  ; multiboot header, will be at the beginning of the binary
 align 4
   dd MAGIC
   dd MBFLAGS
   dd CHECKSUM
 
-section .bss
+section .bss                                        ; uninitialized data (read-write)
 align 16
 stack_bottom:
-resb 16384 ; reserve 16 KiB for the stack
-stack_top:
+resb 16384                                          ; reserve 16 KiB for the stack
+stack_top:                                          ; this is the "beginning" of the stack, as the stack grows downwards
 
-section .text
-global _start:function (_start.end - _start)
+section .text                                       ; entrypoint will be put right after multiboot header in the binary
+
+global _start:function (_start.end - _start)        ; Export _start as a function
+
 _start:
-  mov esp, stack_top
-
+  mov esp, stack_top                                ; initialize stack pointer to the beginning of the stack
+                                                    ; because the stack grows downwards it has to be the
+                                                    ; top of the stack
   extern kernel_main
-  call kernel_main
+  call kernel_main                                  ; continue execution in kernel.c
 
-  cli
+  cli                                               ; prevent any further interrupts
 .hang:
-  jmp .hang
+  jmp .hang                                         ; infinite loop
 .end:
