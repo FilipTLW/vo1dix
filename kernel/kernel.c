a8ce5e67e36049a4fdb45cb16709368aa564fdc6
Filip Myslinski <filipmyslinski2006@gmail.com>
2025-04-15 03:10:54 +0200

    docs: documented kernel

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -29,13 +29,23 @@ enum vga_color {
   VGA_COLOR_WHITE = 15,
 };
 
+/**
+* This function combines a foreground color and a background color and returns the result.
+*/
 static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) { return fg | bg << 4; }
 
+/**
+* This function extends a char with a color value, in the result the lower byte represents the character while the upper
+* byte represents the color.
+*/
 static inline uint16_t vga_entry(unsigned char uc, uint8_t color) { return (uint16_t) uc | (uint16_t) color << 8; }
 
+/**
+* This function calculates the length of a string by counting up until a null character is found.
+*/
 size_t strlen(const char *str) {
   size_t len = 0;
-  while (str[len])
+  while (str[len]) // count up until a null character comes
     len++;
   return len;
 }
@@ -49,6 +59,11 @@ size_t terminal_column;
 uint8_t terminal_color;
 uint16_t *terminal_buffer = (uint16_t *) VGA_MEMORY;
 
+/**
+* Initializes the terminal by setting terminal_row and terminal_column to 0.
+* It also sets the color to black background and light grey foreground.
+* It also clears the whole buffer.
+*/
 void terminal_initialize(void) {
   terminal_row = 0;
   terminal_column = 0;
@@ -62,13 +77,22 @@ void terminal_initialize(void) {
   }
 }
 
+/**
+* Sets the color of characters printed afterwards.
+*/
 void terminal_setcolor(uint8_t color) { terminal_color = color; }
 
+/**
+* Manipulates the buffer by changing a character at (x, y) to c with color color.
+*/
 void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
   const size_t index = y * VGA_WIDTH + x;
   terminal_buffer[index] = vga_entry(c, color);
 }
 
+/**
+* Shifts the whole buffer up a row, clears the bottom row.
+*/
 void terminal_scroll() {
   terminal_row--;
   for (size_t y = 1; y < VGA_HEIGHT; y++) {
@@ -84,6 +108,9 @@ void terminal_scroll() {
   }
 }
 
+/**
+* Prints a single character to the current cursor position. Handles scrolling and the \n character.
+*/
 void terminal_putchar(char c) {
   if (c == '\n') {
     terminal_column = 0;
@@ -101,13 +128,22 @@ void terminal_putchar(char c) {
   }
 }
 
+/**
+* Prints a fixed-size string to the terminal.
+*/
 void terminal_write(const char *data, size_t size) {
   for (size_t i = 0; i < size; i++)
     terminal_putchar(data[i]);
 }
 
+/**
+* Prints a null-terminated string to the terminal.
+*/
 void terminal_writestring(const char *data) { terminal_write(data, strlen(data)); }
 
+/**
+* Entry point for the kernel, called by the bootloader.
+*/
 void kernel_main(void) {
   /* Initialize terminal interface */
   terminal_initialize();
